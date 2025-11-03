f5094f1e67daf3693b61d004686e29f236e1d97c
Julian Salvador <juliansalvador727@gmail.com>
2025-11-02 19:47:17 -0500

    specify python3

--- data/generate_data.py
+++ data/generate_data.py
@@ -3,8 +3,8 @@ import subprocess as sp
 scripts = ["data/borders.py", "data/country_code.py", "data/check.py", "data/matrix.py", "data/matrix_country_code.py"]
 
 for script in scripts:
-    print(f"\Running {script}...")
-    result = sp.run(["python", script], capture_output=True, text=True)
+    print(f"Running {script}...")
+    result = sp.run(["python3", script], capture_output=True, text=True)
     if result.returncode == 0:
         print(f"{script} completed successfully.")
     else:
